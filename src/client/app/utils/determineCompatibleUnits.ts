2969e38026e597340a415570a712d36c5177eef6
Don Truong <dong23102003@gmail.com>
2022-11-24 23:10:13 -0500

    add some functions

--- src/client/app/utils/determineCompatibleUnits.ts
+++ src/client/app/utils/determineCompatibleUnits.ts
@@ -7,19 +7,10 @@ import * as _ from 'lodash';
 import { MeterData } from '../types/redux/meters';
 import { ConversionArray } from '../types/conversionArray';
 import { UnitData, UnitType } from '../types/redux/units';
-import { GroupDefinition } from 'types/redux/groups';
-import { DataType } from 'types/Datasources';
-
-// TODO I put in comments that should start with TODO. I cannot carefully check the code at this point but a number
-// of thoughts are given.
-
-// TODO I'm not sure why this is included.
-import meters from 'reducers/meters';
-
-const Meter = require('../../models/Meter');
-// TODO There should not be test code in production code. I think you want to include the regular DB code.
-const { mocha, expect, testDB } = require('../common');
-
+import { GroupDefinition } from '../types/redux/groups';
+import { DataType } from '../types/Datasources';
+import { State } from '../types/redux/state';
+import { SelectOption } from '../types/items';
 
 /**
  * The intersect operation of two sets.
@@ -96,6 +87,7 @@ export function unitsCompatibleWithUnit(unitId: number): Set<number> {
 	}
 	return unitSet;
 }
+
 /**
  * Returns the row index in Pik for a meter unit.
  * @param unitId The unit id.
@@ -152,118 +144,150 @@ export function metersInGroup(groupId: number): Set<number> {
 	return new Set(group.deepMeters);
 }
 
-// TODO All function documentation needs to be correct TSDoc with type and description for each argument and return types.
 /**
- * Determine the compatibility of meter/group to the current
- * group being worked on (current group)
- * @param currentGroup
+ * Get options for the meter menu on the group page.
+ * @param gid The group's id.
  */
-// TODO If this returns nothing then should it be void?
-// TODO I think this function needs to be exported to be of use. This is why VSC grays it out.
-function compatibilityOfMetersAndGroups(gid: any) {
-	//Get the "currentGroup's" compatible units
-	//Current groups default graphic unit (via Redux)
-	let currentUnits = unitsCompatibleWithMeters(metersInGroup(gid))
+export function getMeterMenuOptionsForGroup(gid: number): SelectOption[] {
+	const state = store.getState() as State;
+	// Get the currentGroup's compatible units.
+	const currentUnits = unitsCompatibleWithMeters(metersInGroup(gid))
+	// Current group's default graphic unit (via Redux).
+	const defaultGraphicUnit = state.groups.byGroupID[gid].defaultGraphicUnit;
+	// Get all meters.
+	// TODO: Why unit not null?
+	const meters = Object.values(state.meters.byMeterID) as MeterData[];
 
-	// current groups defaulft graphic unit (via redux)
-	let currentDefaultGraphicUnit = gid.defaultGraphicUnit
+	// Options for the meter menu.
+	const options: SelectOption[] = [];
 
-	// TODO I'm unclear why the comment for this code from the design document is not present.
-	// TODO This can be done via Redux instead of a DB connection. See _.find above for examples.
-	const conn = testDB.getConnection();
-	let meters = Meter.getUnitNotNull(conn);
+	meters.forEach((meter: MeterData) => {
+		const option = {
+			label: meter.identifier,
+			value: meter.id,
+			isDisabled: false,
+		} as SelectOption;
 
-	meters.forEach(function (m: number) {
-		// TODO I know the design document used case that is not okay but casee if not a good variable name.
-		let casee = compatibleChanges(currentUnits, m, DataType.Meter, currentDefaultGraphicUnit);
-		// TODO cannnot is misspelled.
-		// if case 3 then cannnot select so need logic for that
-		if (casee = 3) {
-
+		const compatibilityChangeCase = getCompatibilityChangeCase(currentUnits, meter.id, DataType.Meter, defaultGraphicUnit);
+		if (compatibilityChangeCase === GroupCase.NoCompatibleUnits) {
+			option.isDisabled = true;
+		} else {
+			// TODO: Handle font
 		}
-		// TODO Lost comment on needing to add to meter menu when that is ready. Follow how done in other parts of the code.
-		// TODO The loop for group seems missing.
+
+		options.push(option);
 	});
 
-
-// TODO What is this commented out code for? I think it is needed to end the function.
-// }
-
-/**
- * Returns the state (see groupCase function) for meter or group 
- * provided by id and otherUnits where type is either DataType.Meter or
- * DataType.group
- */
-function compatibleChanges(otherUnits: Set<number>, id: number, type: DataType, defaultGraphicUnit: number): number{
-	// determine the compatible unites for meter or group represented by id
-	let newUnits;
-	newUnits = compatibleUnits(id, type);
-
-		// TODO OED does not leave a blank line between a comment and code.
-	//Determine case
-
-	let casee = groupCase(otherUnits, newUnits, defaultGraphicUnit); 
-
-	return casee;
+	return options;
 }
 
 /**
- * finds all compatible units for this id based on if meter or group. see compatibleChanges
- * for parameter
- * @param id
- * @param type
+ * Get options for the group menu on the group page.
+ * @param gid The group's id.
  */
-function compatibleUnits(id: number, type: DataType): Set<number> {
-	let newUnits;
-	// TODO The formatting is off. VSC will do this automatically.
-	if(type == DataType.Meter){
-		newUnits = unitsCompatibleWithUnit(id);
-	}else {
-		// TODO It would be nice if comments that are a sentence start with a capital letter and end with a period.
-		//its a group
-		//Note we do this once for each time we check all groups so place to 
-		//optimize if needed.
-		//However, this is done with Redux state so it may be fine to just check each
-		//time and do that for now.
-		newUnits = unitsCompatibleWithMeters(metersInGroup(id)); 
+ export function getGroupMenuOptionsForGroup(gid: number): SelectOption[] {
+	const state = store.getState() as State;
+	// Get the currentGroup's compatible units.
+	const currentUnits = unitsCompatibleWithMeters(metersInGroup(gid))
+	// Current group's default graphic unit (via Redux).
+	const defaultGraphicUnit = state.groups.byGroupID[gid].defaultGraphicUnit;
+	// Get all groups.
+	const groups = Object.values(state.groups.byGroupID) as GroupDefinition[];
+
+	// Options for the group menu.
+	const options: SelectOption[] = [];
+
+	groups.forEach((group: GroupDefinition) => {
+		const option = {
+			label: group.name,
+			value: group.id,
+			isDisabled: false,
+		} as SelectOption;
+
+		const compatibilityChangeCase = getCompatibilityChangeCase(currentUnits, group.id, DataType.Meter, defaultGraphicUnit);
+		if (compatibilityChangeCase === GroupCase.NoCompatibleUnits) {
+			option.isDisabled = true;
+		} else {
+			// TODO: Handle font
+		}
+
+		options.push(option);
+	});
+
+	return options;
+}
+
+/**
+ * The four cases that could happen when adding a group/meter to a group:
+ * 	- NoChange: Adding this meter/group will not change the compatible units for the group. 
+ *  - LostCompatibleUnits: The meter/group is compatible with the default graphic unit although some compatible units are lost.
+ *  - LostDefaultGraphicUnits: The meter/group is not compatible with the default graphic unit but there exists some compatible untis.
+ *  - NoCompatibleUnits: The meter/group will cause the compatible units for the group to be empty.
+ */
+export const enum GroupCase {
+	NoChange = 'NO_CHANGE',
+	LostCompatibleUnits = 'LOST_COMPATIBLE_UNITS',
+	LostDefaultGraphicUnit = 'LOST_DEFAULT_GRAPHIC_UNIT',
+	NoCompatibleUnits = 'NO_COMPATIBLE_UNITS'
+}
+
+/**
+ * Return the case associated if we add the given meter/group to a group.
+ * @param otherUnits The current compatible units of the group
+ * @param id The meter/group's id to add to the group.
+ * @param type Can be METER or GROUP.
+ * @param defaultGraphicUnit The default graphic unit.
+ */
+export function getCompatibilityChangeCase(otherUnits: Set<number>, id: number, type: DataType, defaultGraphicUnit: number): GroupCase {
+	// Determine the compatible units for meter or group represented by the id.
+	const newUnits = getCompatibleUnits(id, type);
+	// Returns the associated case.
+	return groupCase(otherUnits, newUnits, defaultGraphicUnit);
+}
+
+/**
+ * Given a meter or group's id, returns its compatible units.
+ * @param id The meter or group's id.
+ * @param type Can be Meter or Group.
+ */
+function getCompatibleUnits(id: number, type: DataType): Set<number> {
+	if (type == DataType.Meter) {
+		const state = store.getState();
+		// Get the unit id of meter.
+		const unitId = state.meters.byMeterID[id].unitId;
+		// Returns all compatible units with this unit id.
+		return unitsCompatibleWithUnit(unitId);
+	} else {
+		// Note we do this once for each time we check all groups so place to optimize if needed.
+		// However, this is done with Redux state so it may be fine to just check each time and do that for now.
+		return unitsCompatibleWithMeters(metersInGroup(id));
 	}
-
-	return newUnits;
 }
 
 /**
- * Returns case covered above 1, 21, 22 or 3 for cases 1, 2.1, 2.2 or 3.
- * currentUnits should be the units already in group
- * newUnits should be the units that will be added
- * COMPLETED
+ * Returns the group case given current units and new units. See the enum GroupCase for the list of possible cases.
+ * @param currentUnits The current compatible units set.
+ * @param newUnits The new compatible units set.
+ * @param defaultGraphicUnit The default graphic unit.
  */
-function groupCase(currentUnits: Set<number>, newUnits: Set<number>, defaultGraphicUnit: number): number {
-	//The compatible units of a set of meters or groups is the intersection of the compatible units for each
-	//Thus, we can get the units that will go away with (- is set subtraction/difference): 
+function groupCase(currentUnits: Set<number>, newUnits: Set<number>, defaultGraphicUnit: number): GroupCase {
+	// The compatible units of a set of meters or groups is the intersection of the compatible units for each
+	// Thus, we can get the units that will go away with (- is set subtraction/difference):
 	// lostUnit = currentUnit - ( currentUnit n newUnits)
 	let intersection = setIntersect(currentUnits, newUnits);
-
 	let lostUnits = new Set(Array.from(currentUnits).filter(x => !intersection.has(x)));
-	//do the possible cases
-	if (lostUnits.size == 0){
-		// TODO It would be best to use an enum for the return value rather than a fixed value.
-		// no change
-		return 1;
-	}else if (lostUnits.size == currentUnits.size){
-		// no compatible units left
-		return 3;
-		// TODO the second part of the if is missing below.
-	}else if (defaultGraphicUnit != -99){
-		return 22;
-	}else{
-		// if the default graphic unit is no unit then you can add any meter/group
-		return 21;
-	}
 
+	if (lostUnits.size == 0) {
+		return GroupCase.NoChange;
+	} else if (lostUnits.size == currentUnits.size) {
+		return GroupCase.NoCompatibleUnits;
+	} else if (defaultGraphicUnit != -99 && lostUnits.has(defaultGraphicUnit)) {
+		return GroupCase.LostDefaultGraphicUnit;
+	} else {
+		// if the default graphic unit is no unit then you can add any meter/group
+		return GroupCase.LostCompatibleUnits;
+	}
 }
 // TODO The code for somethingLikeFont is missing.
 
 // TODO Who is doing the code for when the selected group/meter is changed?
-
-// TODO I think this } is not needed/in the wrong place.
-}
